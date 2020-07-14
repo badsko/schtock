94d03286ee12098de929256c1592f81f165dfa6d
badsko <badsko@gmail.com>
2020-07-14 21:30:33 +0200

    price variable init before function

--- schtock.py
+++ schtock.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 a = 100
+price = 1
 inc = 'TSLA is now at ${}. Up ${} from closing at ${}.'
 dcr = 'TSLA is now at ${}. Down ${} from closing at ${}.'
 date = datetime.today().isoweekday() < 6
@@ -18,39 +19,39 @@ CHAT_ID = os.getenv('CHAT_ID')
 TELEGRAM_API_SEND_MSG = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 def parsePrice():
-    r=requests.get('https://finance.yahoo.com/quote/TSLA?p=TSLA')
-    soup=bs4.BeautifulSoup(r.text,'lxml')
-    cls='My(6px) Pos(r) smartphone_Mt(6px)'
+    r = requests.get('https://finance.yahoo.com/quote/TSLA?p=TSLA')
+    soup = bs4.BeautifulSoup(r.text,'lxml')
+    cls = 'My(6px) Pos(r) smartphone_Mt(6px)'
     try:
-        price=soup.find('div',{'class': cls}).find('span').text.replace(',','')
+        price = soup.find('div',{'class': cls}).find('span').text.replace(',','')
     except AttributeError:
         print('An error occured')
     return price
 
 def closePrice():
     r=requests.get('https://finance.yahoo.com/quote/TSLA?p=TSLA')
-    soup=bs4.BeautifulSoup(r.text,'lxml')
-    cls='PREV_CLOSE-value'
+    soup = bs4.BeautifulSoup(r.text,'lxml')
+    cls = 'PREV_CLOSE-value'
     try:
-        price=soup.find('td',{'data-test': cls}).find('span').text.replace(',','')
+        price = soup.find('td',{'data-test': cls}).find('span').text.replace(',','')
     except AttributeError:
         print('An error occured')
     return price
 
 while True:
     message_sent = False
-    current = parsePrice()
-    close = closePrice()
+    current = +float(parsePrice())
+    close = +float(closePrice())
     
     if tt and date:
-        if (float(close) + a) <= float(current):
+        if ((close) + a) <= (current):
             if not message_sent:
                 payload = {'chat_id': CHAT_ID, 'text':\
                 inc.format(current, a, close)}
                 r = requests.post(TELEGRAM_API_SEND_MSG, params=payload)
                 message_sent = True
                 time.sleep(5400)
-        elif (float(close) - a) >= float(current):
+        elif ((close) - a) >= (current):
             if not message_sent:
                 payload = {'chat_id': CHAT_ID, 'text':\
                 dcr.format(current, a, close)}
@@ -59,4 +60,4 @@ while True:
                 time.sleep(5400)
     else:
         print (datetime.now().strftime('%H:%M'))
-        time.sleep(60)
+        time.sleep(2)
