4f435de836ecba93aedb8d15fe49af7c57cc5325
badsko <badsko@gmail.com>
2020-07-15 02:21:53 +0200

    Telegram syntax.

--- schtock.py
+++ schtock.py
@@ -7,13 +7,13 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
+TOKEN = os.getenv('TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 a = 100
 p = 1
 url = 'https://www.avanza.se/aktier/om-aktien.html/238449/tesla-inc'
-inc = 'TSLA increased by `${}` from low `${}` now trading at `${}`'
-dcr = 'TSLA decreased by `${}` from high `${}` now trading at `${}` Buy?'
-TOKEN = os.getenv('TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+inc = 'TSLA is now at `${}` Up `${}` from low point of `${}` today.'
+dcr = 'TSLA is now at `${}` Down `${}` from high point of `${}` today.'
 TELEGRAM_API_SEND_MSG = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 def currentPrice():
@@ -42,7 +42,6 @@ while True:
     date = datetime.today().isoweekday() < 6
     tt = datetime.now().strftime('%H:%M') > '13:30' and\
     datetime.now().strftime('%H:%M') < '20:00'
-
     current = +float(currentPrice())
     high = +float(highPrice())
     low = +float(lowPrice())
@@ -51,18 +50,17 @@ while True:
         if ((low) + a) <= (current):
             if not message_sent:
                 payload = {'chat_id': CHAT_ID, 'text':\
-                inc.format(a, low, current), 'parse_mode': 'markdown'}
+                inc.format(current, a, low), 'parse_mode': 'markdown'}
                 r = requests.post(TELEGRAM_API_SEND_MSG, params=payload)
                 message_sent = True
                 time.sleep(60*90)
         elif ((high) - a) >= (current):
             if not message_sent:
                 payload = {'chat_id': CHAT_ID, 'text':\
-                dcr.format(a, high, current), 'parse_mode': 'markdown'}
+                dcr.format(current, a, high), 'parse_mode': 'markdown'}
                 r = requests.post(TELEGRAM_API_SEND_MSG, params=payload)
                 message_sent = True
                 time.sleep(60*90)
-
     else:
         print (datetime.now().strftime('%H:%M'))
         time.sleep(60*5)
