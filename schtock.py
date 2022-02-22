539336f6de5f60ed4b7f1eab44b3e5509b75170c
badsko <badsko@gmail.com>
2022-02-22 10:45:35 +0100

    correcting sleep at night

--- schtock.py
+++ schtock.py
@@ -44,7 +44,7 @@ def main():
             target = datetime(now.year, now.month, now.day, hour=15, minute=30)
             delta = target - now
             ah = datetime(now.year, now.month, now.day, \
-            hour=23, minute=59, second=59)
+            hour=23, minute=54, second=59)
             deltaAfter = ah - now
 
             if not tt and not date:
